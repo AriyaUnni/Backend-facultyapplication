2b34be863fdf9a4e979f511c16bfcf7fbbd880d1
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-20 01:57:40 +0530

    first commit

---
+++ src/main/java/com/nestdigital/facultyapplication/FacultyapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.facultyapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/facultyapplication/FacultyapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.facultyapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
